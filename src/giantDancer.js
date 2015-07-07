d7896d805b4fb23f2674250aa0701ab42f6d5ae3
Hailey Foster <haileyhasmail@gmail.com>
2015-07-07 10:45:27 -0700

    Change color of giant dancer, make it stay centered when it grows.

--- src/giantDancer.js
+++ src/giantDancer.js
@@ -1,6 +1,9 @@
 var GiantDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('giant');
+  this.$node.css({
+    'border-color': '#32cd32'
+  });
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   this.oldStep = this.step;
@@ -8,6 +11,21 @@ var GiantDancer = function(top, left, timeBetweenSteps){
     // call the old version of step at the beginning of any call to this new version of step
     this.oldStep();
     this.$node.toggleClass('giant');
+
+    var position = this.$node.position();
+    var newLeft = position.left;
+    var newTop = position.top;
+    if( this.$node.hasClass('giant') ) {
+      newLeft -= 50;
+      newTop -= 50;
+    } else {
+      newLeft += 50;
+      newTop += 50;
+    }
+    this.$node.css({
+      'left': newLeft,
+      'top': newTop
+    });
   };
 };
 
